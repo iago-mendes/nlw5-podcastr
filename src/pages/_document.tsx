3001b3c2a74ef4c66aba29d4ef98ce1868a01722
Iago <iagobrazmendes@gmail.com>
2021-04-21 17:05:51 -0300

    use favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default class MyDocument extends Document
 		return (
 			<Html>
 				<Head>
+					<link rel='icon' href='/favicon.png' />
+
 					<link rel='preconnect' href='https://fonts.gstatic.com' />
 					<link href='https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap' rel='stylesheet' />
 				</Head>
