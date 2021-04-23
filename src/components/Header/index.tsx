b1001b45a0d235d9ba6a5441b988f2fcb5510c87
Iago <iagobrazmendes@gmail.com>
2021-04-23 14:02:20 -0300

    style theme switcher

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -21,20 +21,22 @@ export function Header()
 
 			<p>The best for you to hear, always</p>
 
-			<Switch
-				checked={isDark}
-				onChange={changeTheme}
-				offColor='#ffe4ad'
-				offHandleColor='#ffad05'
-				uncheckedIcon={<div style={{color: '#ffad05'}} className='themeIcon'><FiSun/></div>}
-				onColor='#44475a'
-				onHandleColor='#6272a4'
-				checkedIcon={<div style={{color: '#6272a4'}} className='themeIcon'><FiMoon/></div>}
-				height={30}
-				width={65}
-			/>
-
-			<span>{currentDate}</span>
+			<div className={styles.group}>
+				<span>{currentDate}</span>
+				
+				<Switch
+					checked={isDark}
+					onChange={changeTheme}
+					offColor='#ffe4ad'
+					offHandleColor='#ffad05'
+					uncheckedIcon={<div style={{color: '#ffad05'}} className={styles.themeIcon}><FiSun/></div>}
+					onColor='#44475a'
+					onHandleColor='#6272a4'
+					checkedIcon={<div style={{color: '#6272a4'}} className={styles.themeIcon}><FiMoon/></div>}
+					height={30}
+					width={65}
+				/>
+			</div>
 		</header>
 	)
 }
